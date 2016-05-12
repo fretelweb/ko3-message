fe837343377a4efdc15793d0946396d4109b127e
Ronny Fretel <ronny@fretelweb.com>
2016-05-11 19:39:29 -0500

    muestro un dialog en ves de un alert

--- views/message/basic.php
+++ views/message/basic.php
@@ -1,12 +1,23 @@
-<div class="<?php echo $message->type ?> alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <?php if (is_array($message->message)): ?>
-    <ul>
-      <?php foreach ($message->message as $msg): ?>
-        <?php echo $msg; ?>
-      <?php endforeach; ?>
-    </ul>
-  <?php else: ?>
-    <?php echo $message->message; ?>
-  <?php endif; ?>
+<div id="ko3msg" class="modal fade">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+         </div>
+         <div class="modal-body">
+            <div class="<?php echo $message->type ?>">
+               <?php if (is_array($message->message)): ?>
+                  <ul>
+                     <?php foreach ($message->message as $msg): ?>
+                        <?php echo $msg; ?>
+                     <?php endforeach; ?>
+                  </ul>
+               <?php else: ?>
+                  <?php echo $message->message; ?>
+               <?php endif; ?>
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
+<script>$('#ko3msg').modal({});</script>
